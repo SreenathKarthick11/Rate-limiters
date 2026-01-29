41752ad27311f4b6d508c67d5dde495d1eef9283
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-01-29 22:09:18 +0530

    updated redis client to read leaky_bucket lua

--- redis_client.py
+++ redis_client.py
@@ -8,14 +8,20 @@ redis_client = redis.Redis(
     decode_responses=True
 )
 
-# Load Lua script
-LUA_SCRIPT_PATH = Path(__file__).parent / "algorithms" / "token_bucket.lua"
+# Load Lua scripts
+LUA_SCRIPT_PATH_TB = Path(__file__).parent / "algorithms" / "token_bucket.lua"
+LUA_SCRIPT_PATH_LB = Path(__file__).parent / "algorithms" / "leaky_bucket.lua"
 
-with open(LUA_SCRIPT_PATH, "r") as f:
+
+with open(LUA_SCRIPT_PATH_TB, "r") as f:
     TOKEN_BUCKET_LUA = f.read()
+
+with open(LUA_SCRIPT_PATH_LB, "r") as f:
+    LEAKY_BUCKET_LUA = f.read()
 
 # Register script (returns callable object)
 token_bucket_script = redis_client.register_script(TOKEN_BUCKET_LUA)
+leaky_bucket_script = redis_client.register_script(LEAKY_BUCKET_LUA)
 
 
 def run_token_bucket(key: str,capacity: int,refill_rate: float,now: int) -> bool:
@@ -32,3 +38,19 @@ def run_token_bucket(key: str,capacity: int,refill_rate: float,now: int) -> bool
     )
 
     return (allowed == 1),remaining,retry_after
+
+
+def run_leaky_bucket(key: str,capacity: int,outflow_rate: float,now: int) -> bool:
+    """
+    Execute leaky bucket rate limiter.
+
+    Returns:
+        True  -> request allowed
+        False -> request denied
+    """
+    allowed,curr_capacity = leaky_bucket_script(
+        keys=[key],
+        args=[capacity, outflow_rate, now]
+    )
+
+    return (allowed == 1),curr_capacity
