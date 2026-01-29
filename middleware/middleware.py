ee7d8bda93ec99ed37ee2aaf2346331dc9bf30ad
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-01-29 22:27:03 +0530

    updated the middleware.py to accomedate future algorithms

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,23 +1,36 @@
 from fastapi import Request, HTTPException, status
 from algorithms.token_bucket import token_bucket_limit
+from algorithms.leaky_bucket import leaky_bucket_limit
 
-def rate_limit(capacity: int, refill_rate: float):
+def rate_limit(type: str, capacity: int, rate: float):
     async def dependency(request: Request):
         identifier = request.client.host
 
-        result = token_bucket_limit(
-            identifier=identifier,
-            capacity=capacity,
-            refill_rate=refill_rate,
-        )
+        if type == "token_bucket":
+            allowed, info = token_bucket_limit(
+                identifier=identifier,
+                capacity=capacity,
+                refill_rate=rate,
+            )
+        elif type == "leaky_bucket":
+            allowed, info = leaky_bucket_limit(
+                identifier=identifier,
+                capacity=capacity,
+                outflow_rate=rate,
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Invalid rate limiter type",
+            )
 
-        if not result.allowed:
+        if not allowed:
             raise HTTPException(
                 status_code=status.HTTP_429_TOO_MANY_REQUESTS,
                 detail="Rate limit exceeded",
-                headers={"Retry-After": str(result.retry_after)},
+                headers={"Retry-After": str(info.get("retry_after"))},
             )
 
-        return result
+        return info
 
     return dependency
