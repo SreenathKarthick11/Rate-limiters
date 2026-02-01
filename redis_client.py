1ca2c8489cacb4c5b016c9c1d9f28a0b63a90c20
SreenathKarthick11 <msreenathkarthick2005@gmail.com>
2026-02-01 18:58:27 +0530

    update redis client to have fwc.

--- redis_client.py
+++ redis_client.py
@@ -11,7 +11,7 @@ redis_client = redis.Redis(
 # Load Lua scripts
 LUA_SCRIPT_PATH_TB = Path(__file__).parent / "algorithms" / "token_bucket.lua"
 LUA_SCRIPT_PATH_LB = Path(__file__).parent / "algorithms" / "leaky_bucket.lua"
-
+LUA_SCRIPT_PATH_FWC = Path(__file__).parent / "algorithms" / "fixed_window_counter.lua"
 
 with open(LUA_SCRIPT_PATH_TB, "r") as f:
     TOKEN_BUCKET_LUA = f.read()
@@ -19,10 +19,13 @@ with open(LUA_SCRIPT_PATH_TB, "r") as f:
 with open(LUA_SCRIPT_PATH_LB, "r") as f:
     LEAKY_BUCKET_LUA = f.read()
 
+with open(LUA_SCRIPT_PATH_FWC, "r") as f:
+    FIXED_WINDOW_COUNTER_LUA = f.read()
+
 # Register script (returns callable object)
 token_bucket_script = redis_client.register_script(TOKEN_BUCKET_LUA)
 leaky_bucket_script = redis_client.register_script(LEAKY_BUCKET_LUA)
-
+fixed_window_counter_script = redis_client.register_script(FIXED_WINDOW_COUNTER_LUA)
 
 def run_token_bucket(key: str,capacity: int,refill_rate: float,now: int) -> bool:
     """
@@ -54,3 +57,19 @@ def run_leaky_bucket(key: str,capacity: int,outflow_rate: float,now: int) -> boo
     )
 
     return (allowed == 1),curr_capacity
+
+
+def run_fixed_window_counter(key: str,capacity: int,window_size: int,now: int) -> bool:
+    """
+    Execute fixed window counter rate limiter.
+
+    Returns:
+        True  -> request allowed
+        False -> request denied
+    """
+    allowed,curr_capacity,retry_after = fixed_window_counter_script(
+        keys=[key],
+        args=[capacity, window_size, now]
+    )
+    
+    return (allowed == 1),curr_capacity,retry_after
